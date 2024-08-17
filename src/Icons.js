51bc8bcb4bb39b666611af15bb24f5a33fc8d8fd
mohammadvaladbiegi2 <mohammadvaladbiegi2@gmail.com>
2024-08-17 12:25:34 +0330

    deploy config

--- src/Icons.js
+++ src/Icons.js
@@ -1,14 +1,14 @@
 import L from 'leaflet';
 
 export const endIcon = new L.Icon({
-    iconUrl: '/end.png',
+    iconUrl: '/mini-snapp-clone/end.png',
     iconSize: [40, 45],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
   });
 export const startIcon = new L.Icon({
-    iconUrl: '/start.png',
+    iconUrl: '/mini-snapp-clone/start.png',
     iconSize: [40, 45],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],

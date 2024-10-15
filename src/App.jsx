7976f7271ba4a12a360561e19f532e34854580e8
mohammad valadbiegi <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2024-10-15 20:46:30 +0330

    Update Map styles

--- src/App.jsx
+++ src/App.jsx
@@ -161,8 +161,12 @@ function App() {
         zoom={13}
         style={{ height: '100vh', width: '100%' }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}"
+          minZoom={0}
+          maxZoom={20}
+          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          ext='png'
         />
         {origin && (
           <Marker

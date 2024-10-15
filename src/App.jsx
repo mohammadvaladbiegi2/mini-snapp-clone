2cf266d890e2d78d1153b5c4976f76badea57765
mohammad valadbiegi <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2024-10-15 20:49:19 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -161,12 +161,8 @@ function App() {
         zoom={13}
         style={{ height: '100vh', width: '100%' }}
       >
-          <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}"
-          minZoom={0}
-          maxZoom={20}
-          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          ext='png'
+     <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {origin && (
           <Marker
